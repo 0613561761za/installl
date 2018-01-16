2af8a8b5c54b961f62cc295f4be35040c0c1ccd0
HerhmoyZ <30932143+0613561761za@users.noreply.github.com>
2018-01-16 06:14:22 +0000

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,77 +1,44 @@
 #!/bin/bash
-
-# Clear terminal first
-clear
-
-# Make sure user is root
-
-if [[ $EUID -ne 0 ]]; then
-   echo "INIT 0!, SCRIPT SHOULD BE RUN AS ROOT!" 1>&2
-   exit 1
-fi
-
-default=Vm0wd2VHUXhTWGhXV0doV
-
-# Installing webserver & configurate
-# Powered by LAMP (Linux, Apche, MySQL, PHP)
-
-# Installing Apache2
-
-apt install apache2 -y
-
-# Installing MySQL Server
-
-apt install debconf-utils -y
-echo "mysql-server mysql-server/root_password select $default" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again select $default" | debconf-set-selections
-sudo apt-get -y install mysql-server
-
-# Installing PHP
-
-sudo apt install libapache2-mod-php php-mcrypt php-mysql php-mbstring php-curl php-tokenizer php-xml
-
-#######################################################
-#           WEBSERVER SUCCESSFULLY INSTALLED          #
-#######################################################
-
-#######################################################
-#                CONFIGURING SSHPANEL                 #
-#######################################################
-
-# Changed webserver root directory
+apt-get update && apt-get upgrade -y
+apt-get install curl -y
+apt-get install apache2 -y
+apt-get install php5 libapache2-mod-php5 php5-mcrypt -y
+service apache2 restart
+apt-get install mysql-server php5-mysql -y
+mysql_install_db
+mysql_secure_installation
+apt-get install phpmyadmin -y
+php5enmod mcrypt
+service apache2 restart
+ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
+apt-get install libssh2-1-dev libssh2-php -y
+apt-get install libapache2-mod-php php-mcrypt php-mysql php-mbstring php-curl php-tokenizer php-xml
+mysql -u root -p 52tm -e "CREATE DATABASE sshpanel"
+php -m |grep ssh2
+service apache2 restart
 apt install sed
 sed -i 's/var\/www/var\/www\/html\/sshpanel\/public/g' /etc/apache2/apache2.conf
-
-# Go to Webserver dir
 cd /var/www/html
-
-# Cloning SSHPANEL from GitHub
-# Make sure Git has been installed
 apt install git -y
-
-# Cloning!
 git clone git://github.com/0613561761za/sshpanel.git
-
-# Generating .env file & configure database
-mysql -u root -p$default -e "CREATE DATABASE IF NOT EXISTS sshpanel;exit;"
 cd /var/www/html/sshpanel
 echo "DB_CONNECTION=mysql" >> .env
 echo "DB_PORT=3306" >> .env
 echo "DB_DATABASE=sshpanel" >> .env
 echo "DB_USERNAME=root" >> .env
-echo "DB_PASSWORD=$default" >> .env
-mysql -u root -p$default -e "use sshpanel; SET autocommit =0; source database/database.sql; COMMIT; exit;"
+echo "DB_PASSWORD=52tm" >> .env
+mysql -u root -p 52tm -e "use sshpanel; SET autocommit =0; source database/database.sql; COMMIT; exit;"
 
 
 
 chmod -R 755 /var/www/html/sshpanel/*
 chmod -R 755 /var/www/html/sshpanel/storage
 
-#Restarting webserver
-sudo apache2 restart
+rm index.html
+clear
 
-# Complete installation
-clear 
+service apache2 restart
+
 
 
 # Tell user installation is complete
